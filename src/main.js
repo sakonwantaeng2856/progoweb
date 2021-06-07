975d96666d5e6c41e4ea4bde65743448167ec61a
sakon2856 <68525340+sakonwantaeng2856@users.noreply.github.com>
2021-06-07 23:35:50 +0700

    progoweb

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import VueRouter from 'vue-router';
-import Vue from 'vue';
 
-Vue.use(VueRouter);
-createApp(App).use(store).use(router).mount("#app");                               
+
+createApp(App).use(store).use(router).mount("#app");
